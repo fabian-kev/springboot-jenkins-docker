dd5d895fa93bed06c5d25374917a26ca997f32f2
k.fabian <akame1v33@gmail.com>
2021-04-27 21:29:20 +0800

    Add a new field 'skills'

--- src/main/java/com/fabiankevin/jenkins/jenkinswithdocker/Player.java
+++ src/main/java/com/fabiankevin/jenkins/jenkinswithdocker/Player.java
@@ -3,12 +3,15 @@ package com.fabiankevin.jenkins.jenkinswithdocker;
 import lombok.Builder;
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 @Builder
 public class Player {
     private Long id;
     private String name;
     private Character character;
+    private List<String> skills;
 }
 
 @Data
